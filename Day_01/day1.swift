2f398c606162bd62022f68d131c2f0237781a36b
Ben Kennedy <ben@zygoat.ca>
2018-12-01 19:39:52 -0800

    Advent of Code! Day 1.
    Props to https://github.com/misstonbon/advent-of-py-2018. :D

---
+++ Day_01/day1.swift
@@ -0,0 +1,28 @@
+// Advent of Code, Day 1
+//
+// ben@zygoat.ca
+
+import Foundation
+
+let fileURL = URL(fileURLWithPath: "./freq_deltas.txt")
+let deltas = try! String.init(contentsOf: fileURL).split(separator: "\n").map { Int($0)! }
+
+var accumulatedFreqs = Set<Int>()
+var currentFreq = 0
+var throughOnce = false
+
+repeat {
+    for n in deltas {
+        accumulatedFreqs.insert(currentFreq)
+        currentFreq += n
+        if accumulatedFreqs.contains(currentFreq) {
+            print("part 2. first dupe freq: \(currentFreq)")  // 82516
+            exit(0)
+        }
+    }
+    
+    if !throughOnce {
+        print("part 1. end freq after first iteration: \(currentFreq)")  // 578
+        throughOnce = true
+    }
+} while true
