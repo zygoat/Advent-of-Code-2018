a303f89df46dafa626c7dc649583851e26d7847c
Ben Kennedy <ben@zygoat.ca>
2018-12-03 00:04:03 -0800

    Day 3, part 2!

--- Day_03/fabric.swift
+++ Day_03/fabric.swift
@@ -7,45 +7,51 @@ import Foundation
 let fileURL = URL(fileURLWithPath: "./claims.txt")
 let manifest = try! String.init(contentsOf: fileURL).split(separator: "\n")
 
-func rectFromManifest(_ substring: Substring) -> (x: Int, y: Int, w: Int, h: Int) {
-    var n: Int = 0
-    var x: Int = 0
-    var y: Int = 0
-    var w: Int = 0
-    var h: Int = 0
-    let scanner = Scanner(string: String(substring))
-    scanner.charactersToBeSkipped = CharacterSet.decimalDigits.inverted
-    scanner.scanInt(&n) // ignore
-    scanner.scanInt(&x)
-    scanner.scanInt(&y)
-    scanner.scanInt(&w)
-    scanner.scanInt(&h)
-    return (x, y, w, h)
+struct Claim {
+    let number: Int
+    let x: Int
+    let y: Int
+    let w: Int
+    let h: Int
+
+    init(from substring: Substring) {
+        let scanner = Scanner(string: String(substring))
+        scanner.charactersToBeSkipped = CharacterSet.decimalDigits.inverted
+        var n: Int = 0 ; scanner.scanInt(&n) ; self.number = n
+        var x: Int = 0 ; scanner.scanInt(&x) ; self.x = x
+        var y: Int = 0 ; scanner.scanInt(&y) ; self.y = y
+        var w: Int = 0 ; scanner.scanInt(&w) ; self.w = w
+        var h: Int = 0 ; scanner.scanInt(&h) ; self.h = h
+    }
 }
 
-var occupations = [Int : [Int : Int]]()   // [y : [x : count]]
+var distinctClaimNumbers = Set<Int>()
+var occupations = [Int : [Int : [Claim]]]()   // [y : [x : [claim]]]
+var overlapCount = 0
 
 for entry in manifest {
-    let rect = rectFromManifest(entry)
-
-    for y in (rect.y ..< rect.y + rect.h) {
-        var row = occupations[y] ?? [Int : Int]()
+    let claim = Claim(from: entry)
+    distinctClaimNumbers.insert(claim.number)
+    
+    for y in (claim.y ..< claim.y + claim.h) {
+        var row = occupations[y] ?? [Int : [Claim]]()
         
-        for x in (rect.x ..< rect.x + rect.w) {
-            row[x, default: 0] += 1
+        for x in (claim.x ..< claim.x + claim.w) {
+            var position = row[x] ?? [Claim]()
+            position.append(claim)
+            if position.count > 1 {
+                if position.count == 2 {
+                    overlapCount += 1
+                }
+                for claim in position {
+                    distinctClaimNumbers.remove(claim.number)
+                }
+            }
+            row[x] = position
             occupations[y] = row
         }
     }
 }
 
-var overlapCount = 0
-
-for (_, row) in occupations {
-    for (_, count) in row {
-        if count > 1 {
-            overlapCount += 1
-        }
-    }
-}
-
 print("Part 1. Total square inches under multiple claims: \(overlapCount)")  // 107043
+print("Part 2. Claim numbers that have no overlaps: \(distinctClaimNumbers)")  // 346
