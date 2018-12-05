da53c27958de5672765cadebf99e386e2e56e179
Ben Kennedy <ben@zygoat.ca>
2018-12-05 00:22:46 -0800

    Day 5, part 2!
    Total execution time is about 90 seconds on this computer. Still seems pretty slow. Hmm…

--- Day_05/reaction.swift
+++ Day_05/reaction.swift
@@ -7,19 +7,39 @@ import Foundation
 let fileURL = URL(fileURLWithPath: "./polymer.txt")
 let polymer = try! String.init(contentsOf: fileURL).replacingOccurrences(of: "\n", with: "")
 
-var reducedPolymer = polymer
-var lastReduction = ""
+let alphabet = UnicodeScalar("a").value ... UnicodeScalar("z").value
 
-repeat {
-    lastReduction = reducedPolymer
-    for n in UnicodeScalar("a").value ... UnicodeScalar("z").value {
-        let c = UnicodeScalar(n)!
-        let lower = String(c)
-        let upper = String(c).uppercased()
-        for search in [(lower + upper), (upper + lower)] {
-            reducedPolymer = reducedPolymer.replacingOccurrences(of: search, with: "")
+func reduce(_ polymer: String) -> Int {
+    var reducedPolymer = polymer
+    var lastReduction = ""
+    
+    repeat {
+        lastReduction = reducedPolymer
+        for n in alphabet {
+            let c = UnicodeScalar(n)!
+            let lower = String(c)
+            let upper = String(c).uppercased()
+            for search in [(lower + upper), (upper + lower)] {
+                reducedPolymer = reducedPolymer.replacingOccurrences(of: search, with: "")
+            }
         }
-    }
-} while reducedPolymer != lastReduction
+    } while reducedPolymer != lastReduction
+    
+    return reducedPolymer.count
+}
 
-print("Part 1. The reduced polymer is \(reducedPolymer.count) units long.")
+print("Part 1. The reduced initial polymer is \(reduce(polymer)) units long.")  // 10598
+
+var shortestLength = Int.max
+
+for n in alphabet {
+    let search = String(UnicodeScalar(n)!)
+    let modifiedPolymer = polymer.replacingOccurrences(of: search, with: "", options: .caseInsensitive)
+    let length = reduce(modifiedPolymer)
+    
+    if length < shortestLength {
+        shortestLength = length
+    }
+}
+
+print("Part 2. The shortest reduced polymer is \(shortestLength) units long.")  // 5312 (removing "j", nearly 50% shorter!
